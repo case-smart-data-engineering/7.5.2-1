c0a81ad8892cd08c7c81ac5ba7dfe2f6dff6d56c
blizzard-w <310946205@qq.com>
2023-01-07 14:22:44 +0000

    update

--- 3_自测练习/my_solution.py
+++ 3_自测练习/my_solution.py
@@ -66,6 +66,7 @@ parser.add_argument('--N_train', nargs='?', const=1, type=int, default=10)
 parser.add_argument('--N_test', nargs='?', const=1, type=int, default=10)
 parser.add_argument('--lr', nargs='?', const=1, type=float, default=0.004)
 
+
 args = parser.parse_args()
 
 if torch.cuda.is_available():
@@ -187,85 +188,7 @@ def test(gnn, gen, n_classes, iters=args.num_examples_test):
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-def diaoyong():
-    gen = Generator()
-    gen.N_train = args.N_train
-    gen.N_test = args.N_test
-    gen.edge_density = args.edge_density
-    gen.p_SBM = args.p_SBM
-    gen.q_SBM = args.q_SBM
-    gen.random_noise = args.random_noise
-    gen.noise = args.noise
-    gen.noise_model = args.noise_model
-    gen.generative_model = args.generative_model
-    gen.n_classes = args.n_classes
-
-
-    torch.backends.cudnn.enabled=False
-
-    if (args.mode == 'test'):
-        print ('In testing mode')
-        filename = args.filename_existing_gnn
-        path_plus_name = os.path.join(args.path_gnn, filename)
-        if ((filename != '') and (os.path.exists(path_plus_name))):
-            print ('Loading gnn ' + filename)
-            gnn = torch.load(path_plus_name)
-            if torch.cuda.is_available():
-                gnn.cuda()
-        else:
-            print ('No such a gnn exists; creating a brand new one')
-            if (args.generative_model == 'SBM_multiclass'):
-                gnn = GNN_multiclass(args.num_features, args.num_layers, args.J + 2, n_classes=args.n_classes)
-            filename = 'gnn_J' + str(args.J) + '_lyr' + str(args.num_layers) + '_Ntr' + str(args.N_train) + '_num' + str(args.num_examples_train)
-            path_plus_name = os.path.join(args.path_gnn, filename)
-            if torch.cuda.is_available():
-                gnn.cuda()
-            print ('Training begins')
-
-
-    elif (args.mode == 'train'):
-        filename = args.filename_existing_gnn
-        path_plus_name = os.path.join(args.path_gnn, filename)
-        if ((filename != '') and (os.path.exists(path_plus_name))):
-            print ('Loading gnn ' + filename)
-            gnn = torch.load(path_plus_name)
-            filename = filename + '_Ntr' + str(args.N_train) + '_num' + str(args.num_examples_train)
-            path_plus_name = os.path.join(args.path_gnn, filename)
-        else:
-            print ('No such a gnn exists; creating a brand new one')
-            filename = 'gnn_J' + str(args.J) + '_lyr' + str(args.num_layers) + '_Ntr' + str(args.N_train) + '_num' + str(args.num_examples_train)
-            path_plus_name = os.path.join(args.path_gnn, filename)
-            if (args.generative_model == 'SBM_multiclass'):
-                gnn = GNN_multiclass(args.num_features, args.num_layers, args.J + 2, n_classes=args.n_classes)
-
-        print ('total num of params:', count_parameters(gnn))
-
-        if torch.cuda.is_available():
-            gnn.cuda()
-        print ('Training begins')
-        if (args.generative_model == 'SBM_multiclass'):
-            train(gnn, gen, args.n_classes)
-        print ('Saving gnn ' + filename)
-        if torch.cuda.is_available():
-            torch.save(gnn.cpu(), path_plus_name)
-            gnn.cuda()
-        else:
-            torch.save(gnn, path_plus_name)
-
-
-    print ('Testing the GNN:')
-    if args.eval_vs_train:
-        print ('model status: eval')
-        gnn.eval()
-    else:
-        print ('model status: train')
-        gnn.train()
-
-    test(gnn, gen, args.n_classes)
-
-    print ('total num of params:', count_parameters(gnn))
-
 if __name__ == '__main__':
-  pass
+    pass
 
 
--- 3_自测练习/test_case.py
+++ 3_自测练习/test_case.py
@@ -5,5 +5,18 @@ from my_solution import *
 
 # 测试用例
 def test_solution():
-    diaoyong()
-
+    gen = Generator()
+    gen.N_train =10
+    gen.N_test =10
+    gen.edge_density =0.2
+    gen.p_SBM =0.0
+    gen.q_SBM =0.045
+    gen.noise = 0.03
+    gen.noise_model =2
+    gen.generative_model ='SBM_multiclass'
+    gen.n_classes = 2
+    torch.backends.cudnn.enabled=False
+    gnn = GNN_multiclass(10, 1, 2 + 2, n_classes=2)
+    print ('Testing the GNN:')
+    test(gnn, gen, 2)
+    print ('total num of params:', count_parameters(gnn))
